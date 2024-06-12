08b058c995416f98a8310a025b0b08753984055e
KVsixtynine <156992474+KVsixtynine@users.noreply.github.com>
2024-06-12 07:46:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,20 @@ app.listen(PORT, () => {
 });
 
 module.exports = app; 
+
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/alvdansen/BandW-Manga",
+		{
+			headers: { Authorization: "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+query({"inputs": "Astronaut riding a horse"}).then((response) => {
+	// Use image
+});

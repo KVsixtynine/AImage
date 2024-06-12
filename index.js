7bb8daadb2152abf4a0b773e3a523ec55c48172c
KVsixtynine <156992474+KVsixtynine@users.noreply.github.com>
2024-06-11 22:31:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Client } from "@gradio/client";
+
 
 const app = express();
 
@@ -9,25 +9,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-async function connectToClient() {
-  const client = await Client.connect("prithivMLmods/DALLE-4K");
-  const result = await client.predict("/run", {
-    prompt: "Hello!!",
-    negative_prompt: "Hello!!",
-    use_negative_prompt: true,
-    style: "3840 x 2160",
-    seed: 0,
-    width: 512,
-    height: 512,
-    guidance_scale: 0.1,
-    randomize_seed: true,
-  });
-  console.log(result.data);
-}
 
-connectToClient().catch(error => {
-  console.error(error);
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
